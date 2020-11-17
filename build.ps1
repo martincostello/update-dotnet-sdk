5674a3f93649a016adbf783cbcdb6498eaf1c045
martincostello <martin@martincostello.com>
2020-11-17 18:14:49 +0000

    Stop build on error
    Stop build.ps1 on the first error to not ignore issues from ncc (or the tests).

--- build.ps1
+++ build.ps1
@@ -1,3 +1,15 @@
 #! /usr/bin/pwsh
+
+$ErrorActionPreference = "Stop"
+
 npm run build
+
+if ($LASTEXITCODE -ne 0) {
+    throw "build failed"
+}
+
 npm run test
+
+if ($LASTEXITCODE -ne 0) {
+    throw "test failed"
+}
