798241bf4f3fc156b848f078924ea7d6d0eeb653
martincostello <martin@martincostello.com>
2022-07-16 11:35:14 +0100

    Rename entry point
    Rename the entry point to be `main.js`.

--- dist/index.js
+++ dist/index.js
@@ -354,7 +354,7 @@ class NullWritable extends stream_1.Writable {
 
 /***/ }),
 
-/***/ 6956:
+/***/ 3109:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
@@ -10733,7 +10733,7 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __nccwpck_require__(6956);
+/******/ 	var __webpack_exports__ = __nccwpck_require__(3109);
 /******/ 	module.exports = __webpack_exports__;
 /******/ 	
 /******/ })()
